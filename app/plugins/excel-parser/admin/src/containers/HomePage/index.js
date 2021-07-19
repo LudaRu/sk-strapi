fb16959794ec5a8c90cda3f2e3b1c4a584595197
Rus K <rus@example.com>
2021-07-19 08:12:19 +0300

    x

--- app/plugins/excel-parser/admin/src/containers/HomePage/index.js
+++ app/plugins/excel-parser/admin/src/containers/HomePage/index.js
@@ -52,30 +52,30 @@ const HomePage = () => {
       });
 
       console.log('XLSX_CALC', XLSX_CALC)
-      for (let i = 1; i < 50; i++) { // выведет 0, затем 1, затем 2
-        wb.Sheets['Калькулятор каркас'].T8.v = i
+      for (let i = 1; i < 50; i++) {
+        wb.Sheets['Калькулятор каркас'].S27.v = i
         XLSX_CALC.XLSX_CALC(wb)
         const ws = wb.Sheets['Калькулятор каркас']
 
-        if (ws.E5.v == 0) {
+        if (ws.T27.v == 0) {
           break
         }
 
         const updateData = {
-          price_1: ws.Z3.v.toFixed(),
+          price_1: ws.AD22.v.toFixed(),
           discount: 0,
-          opt_size_bani_w: ws.E6.v,
-          opt_size_bani_h: ws.F6.v,
-          opt_size_veranda_w: ws.E7.v,
-          opt_size_veranda_h: ws.F7.v,
-          opt_size_parnoi_w: ws.E8.v,
-          opt_size_parnoi_h: ws.F8.v,
+          opt_size_bani_w: ws.D25.v,
+          opt_size_bani_h: ws.E25.v,
+          opt_size_veranda_w: ws.D26.v,
+          opt_size_veranda_h: ws.E26.v,
+          // opt_size_parnoi_w: ws.D27.v,
+          // opt_size_parnoi_h: ws.E27.v,
 
-          opt_count_rooms: ws.E9.v, // Общее Кол-во помещений (вкл веранду)
-          opt_size_wall: ws.E10.v, // Общая длинна перегородок
-          opt_dot_foundation: ws.E11.v, // Количество точек фундамент.
-          opt_ceiling_height: ws.E12.v, // Высота потолка.
-          opt_roof_area: ws.E13.v, // Площадь кровли
+          // opt_count_rooms: ws.E9.v, // Общее Кол-во помещений (вкл веранду)
+          // opt_size_wall: ws.E10.v, // Общая длинна перегородок
+          // opt_dot_foundation: ws.E11.v, // Количество точек фундамент.
+          // opt_ceiling_height: ws.E12.v, // Высота потолка.
+          // opt_roof_area: ws.E13.v, // Площадь кровли
 
           kits: {
             фундамент: [],
@@ -89,36 +89,30 @@ const HomePage = () => {
               бак: [],
             },
             пожарная: [],
-            отделка: {
-              комфорт: {
-                price: 0,
-                items: [],
-              },
-              премиум: {
-                price: 0,
-                items: [],
-              },
-              люкс: {
-                price: 0,
-                items: [],
-              }
-            }
+            отделка: {}
           }
         }
 
-        const START_ROW = 64
+        const START_ROW = 60
         let currCat = ''
         for (let i = 0; i < 300; i++) {
           const pos = START_ROW + i
 
-          if (!ws['O' + pos]) {
+          if (!ws['N' + pos]) {
             break
           }
 
+          // конец цен
+          if ( ws['N' + pos].v === 'РАСХОДЫ ПО ФУНДАМЕНТУ') {
+            break;
+          }
+
           // Категория
-          if (categories.indexOf(ws['O' + pos].v) !== -1) {
-            currCat = ws['O' + pos].v;
+          if (categories.indexOf(ws['N' + pos].v) !== -1) {
+            currCat = ws['N' + pos].v;
           } else {
+
+
 
             // Отделка
             if ([
@@ -127,7 +121,7 @@ const HomePage = () => {
               'УТЕПЛЕНИЕ',
               'ВНЕШНЯЯ ОТДЕЛКА',
               'НАСТИЛ ПОЛА',
-              'ВНУТРЕННЯЯ ОТТДЕЛКА',
+              'ВНУТРЕННЯЯ ОТТДЕЛКА ',
               'ОКНА',
               'ДВЕРИ',
               'ПОЛОГА В ПАРНОЙ',
@@ -135,41 +129,48 @@ const HomePage = () => {
               'КОММУНИКАЦИИ, БЛАГОУСТРОЙСТВО',
               'ЗАЩИТА СТРОЕНИЯ ОТ ВНЕШНИХ ФАКТОРОВ'
             ].indexOf(currCat) !== -1) {
-              console.log('ws', ws)
-              if (ws['AA' + pos].v) {
-                updateData.kits['отделка']['комфорт'].items.push({
-                  category: currCat.normalize(),
-                  name: ws['O' + pos].v,
-                  price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
-                })
-
-              } else if (ws['AB' + pos].v) {
-                updateData.kits['отделка']['премиум'].items.push({
-                  category: currCat.normalize(),
-                  name: ws['O' + pos].v,
-                  price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
-                })
-
-              } else if (ws['AC' + pos].v) {
-                updateData.kits['отделка']['люкс'].items.push({
-                  category: currCat.normalize(),
-                  name: ws['O' + pos].v,
-                  price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
-                })
+              if (
+                [
+                  'ДВЕРИ',
+                  'КОММУНИКАЦИИ, БЛАГОУСТРОЙСТВО',
+                  'УТЕПЛЕНИЕ',
+                  'КОНСТРУКТИВ ОСНОВАНИЯ КАРКАСА',
+                  'ЗАЩИТА СТРОЕНИЯ ОТ ВНЕШНИХ ФАКТОРОВ',
+                ].indexOf(currCat) !== -1
+              ) {
+                if (!updateData.kits['отделка'][currCat.toLowerCase()]) {
+                  updateData.kits['отделка'][currCat.toLowerCase()] = {
+                    multiple: true,
+                    list: []
+                  }
+                }
+              } else {
+                if (!updateData.kits['отделка'][currCat.toLowerCase()]) {
+                  updateData.kits['отделка'][currCat.toLowerCase()] = {
+                    multiple: false,
+                    list: []
+                  }
+                }
               }
+
+              updateData.kits['отделка'][currCat.toLowerCase()].list.push({
+                name: ws['N' + pos].v,
+                price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
+              })
+
               // ФУНДАМЕНТ
             } else if (['ФУНДАМЕНТ'].indexOf(currCat) !== -1) {
 
               updateData.kits['фундамент'].push({
-                name: ws['O' + pos].v,
-                price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                name: ws['N' + pos].v,
+                price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
               })
               // ПОЖАРНАЯ
             } else if (['ПОЖАРНАЯ БЕЗОПАСНОСТЬ'].indexOf(currCat) !== -1) {
 
               updateData.kits['пожарная'].push({
-                name: ws['O' + pos].v,
-                price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                name: ws['N' + pos].v,
+                price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
               })
               // Печи
             } else if ([
@@ -182,45 +183,38 @@ const HomePage = () => {
               switch (currCat) {
                 case 'ПЕЧЬ СТАЛЬНАЯ':
                   updateData.kits['печное']['печь']['сталь'].push({
-                    name: ws['O' + pos].v,
-                    price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                    name: ws['N' + pos].v,
+                    price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
                   })
                   break;
                 case 'ПЕЧЬ ЧУГУННАЯ':
                   updateData.kits['печное']['печь']['чугун'].push({
-                    name: ws['O' + pos].v,
-                    price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                    name: ws['N' + pos].v,
+                    price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
                   })
                   break;
                 case 'БАК':
                   updateData.kits['печное']['бак'].push({
-                    name: ws['O' + pos].v,
-                    price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                    name: ws['N' + pos].v,
+                    price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
                   })
                   break;
                 case 'ОБУСТРОЙСТВО ПЕЧИ':
                   updateData.kits['печное']['обустройство'].push({
-                    name: ws['O' + pos].v,
-                    price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                    name: ws['N' + pos].v,
+                    price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
                   })
                   break;
                 case 'ДЫМОХОД':
                   updateData.kits['печное']['дымоход'].push({
-                    name: ws['O' + pos].v,
-                    price: +(+ws['K' + pos].v * +ws['M' + pos].v).toFixed(),
+                    name: ws['N' + pos].v,
+                    price: +(+ws['J' + pos].v * +ws['L' + pos].v).toFixed(),
                   })
                   break;
                 default:
               }
             }
           }
-        }
-
-        for (const kitType in updateData.kits['отделка']) {
-          updateData.kits['отделка'][kitType].items.forEach((el, i) => {
-            updateData.kits['отделка'][kitType].price += +updateData.kits['отделка'][kitType].items[i].price
-            delete updateData.kits['отделка'][kitType].items[i].price
-          })
         }
 
         console.log(updateData)
@@ -241,123 +235,6 @@ const HomePage = () => {
           })
       }
       setPars('Готово')
-
-      //
-      //
-      // // Обновление комплектаций
-      // wb.SheetNames.forEach(wsName => {
-      //   const projectCode = wsName.split(' ')[0].split('-')
-      //
-      //   if (typeof projectCode[0] === 'string' && Number(projectCode[1])) {
-      //     const ws = wb.Sheets[wsName];
-      //
-      //     console.log(wsName, ws)
-      //
-      //     const updateData = {
-      //       base: [
-      //         {category: 'Нижняя Обвязка', name: 'Брус 100х100 мм. Хвоя'},
-      //         {category: 'Лаги пола', name: 'Доска 40х100мм. Ест. Влажности'},
-      //         {category: 'Каркас стен', name: 'Доска 40х100мм. Ест. Влажности'},
-      //         {category: 'Изоляция стен', name: 'Ветро,пароизоляция, Изофлекс"А""В"'},
-      //         {category: 'Утепление', name: 'Толщина 50мм. Только парное отделение'},
-      //         {category: 'Утеплитель', name: 'Утеплитель Рулонный "Кнауф", "Изовер"'},
-      //         {category: 'Внешняя отделка', name: 'Евровагонка сорт "ВС"'},
-      //         {category: 'Кровля покрытие', name: 'Профлист С-10 Оцынкованный'},
-      //         {category: 'Внутренняя отделка', name: 'Стены и потолки вагонка хвоя сорт "ВС"'},
-      //         {category: 'Полы по бане', name: 'Доска Строганная 40мм. Естественной влажности, хвоя'},
-      //         {category: 'Окна', name: 'Деревянные в 1 стекло без открывания'},
-      //         {category: 'Двери', name: 'Деревянные банные "ласточкин хвост", хвоя'},
-      //       ],
-      //       kit_1: [],
-      //       kit_2: [],
-      //       kit_3: [],
-      //     }
-      //
-      //     fetch(`/banis?number=${projectCode[1]}`)
-      //       .then(response => response.json())
-      //       .then(data => {
-      //         if (data.length) {
-      //
-      //           // парсинг комплектаций
-      //           const START_POS = 43
-      //
-      //           let CURRENT_CAT_I = -1
-      //
-      //           for (let i = 0; i < 200; i++) {
-      //             const pos = START_POS + i
-      //             if (!ws['M' + pos]) {
-      //               break
-      //             }
-      //
-      //             // Категория
-      //             if (ws['M' + pos].v && ws['M' + pos].s.fgColor.rgb === 'DEEBF7') {
-      //               console.log(ws['M' + pos].v)
-      //               // Удаление пустых категорий
-      //               // if (CURRENT_CAT_I !== -1 && updateData.kit_1[CURRENT_CAT_I] && updateData.kit_1[CURRENT_CAT_I].items.length  ) {
-      //               //
-      //               // }
-      //
-      //               CURRENT_CAT_I = CURRENT_CAT_I + 1
-      //               updateData.kit_1[CURRENT_CAT_I] = {
-      //                 category: ws['M' + pos].v,
-      //                 items: []
-      //               }
-      //               updateData.kit_2[CURRENT_CAT_I] = {
-      //                 category: ws['M' + pos].v,
-      //                 items: []
-      //               }
-      //               updateData.kit_3[CURRENT_CAT_I] = {
-      //                 category: ws['M' + pos].v,
-      //                 items: []
-      //               }
-      //
-      //             }
-      //
-      //             // Подкатегория
-      //             if (ws['M' + pos].v && ws['M' + pos].s.fgColor.rgb !== 'DEEBF7') {
-      //               console.log('CURRENT_CAT_I', CURRENT_CAT_I)
-      //               // kit 1
-      //               if (ws['X' + pos] && ws['X' + pos].v) {
-      //                 updateData.kit_1[CURRENT_CAT_I].items.push(
-      //                   {name: ws['M' + pos].v}
-      //                 )
-      //               }
-      //
-      //               // kit 2
-      //               if (ws['Y' + pos] && ws['Y' + pos].v) {
-      //                 updateData.kit_2[CURRENT_CAT_I].items.push(
-      //                   {name: ws['M' + pos].v}
-      //                 )
-      //               }
-      //
-      //               // kit 3
-      //               if (ws['Z' + pos] && ws['Z' + pos].v) {
-      //                 updateData.kit_3[CURRENT_CAT_I].items.push(
-      //                   {name: ws['M' + pos].v}
-      //                 )
-      //               }
-      //             }
-      //           }
-      //
-      //
-      //           console.log('updateData', {kits: updateData})
-      //
-      //           setPars('Загрузка...')
-      //           fetch(`/banis/${data[0].id}`, {
-      //             headers: {
-      //               'Authorization': 'Bearer ' + auth.getToken(),
-      //               'Content-Type': 'application/json'
-      //             },
-      //             withCredentials: true,
-      //             credentials: 'include',
-      //             method: 'PUT',
-      //             body: JSON.stringify({kits: updateData}),
-      //           }).then(r => setPars('Готово'))
-      //
-      //         }
-      //       });
-      //   }
-      // })
 
     };
   };
